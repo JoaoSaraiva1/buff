f759a3a0a98d5216791dbd20f537474e61875330
João Saraiva <joaosaiva.s.js@gmail.com>
2024-01-06 18:26:17 +0000

    Update VSCode settings and page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { prisma } from "../../prisma/index";
 
 export default async function Home() {
-  const allItems = await prisma.items.findMany();
-  console.log(allItems);
+  const items = await prisma.price_history.findMany();
+
+  console.log("🚀 ~ file: page.tsx:5 ~ Home ~ items:", items);
 
   return (
-    <div>
+    <div className="bg-orange-300 w-screen h-screen">
       <h1>Home</h1>
     </div>
   );
